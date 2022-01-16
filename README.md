d13e90d7dcc4ff8025f604d5554e84593527cab7
Ian Vaz Araujo <ianaraujo15@gmail.com>
2022-01-15 21:23:04 -0300

    Add image to README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # migracao-partidaria
 Visualizando a migração de candidatos entre eleições na cidade do Rio de Janeiro.
+
+![Figura 1](/viz/diagrama_arco.jpeg)
--- main.R
+++ main.R
@@ -1,6 +1,7 @@
 
 library(tidyverse)
 library(magrittr)
+
 library(electionsBR)
 
 # Set timeout limit to 10 minutes due to data download from TSE's repo
@@ -25,7 +26,7 @@ elec_2020 <- read_rds(file = "elec_2020.rds")
 elec_2016 <- elec_2016[, names(elec_2016) != "NR_PROTOCOLO_CANDIDATURA"]
 elec_2020 <- elec_2020[, names(elec_2020) != "NR_PROTOCOLO_CANDIDATURA"]
 
-# Data transformations...
+# Binding the data and later transforming it to the wide format
 
 data <- bind_rows(elec_2016, elec_2020)
 
@@ -33,7 +34,26 @@ data %<>%
   filter(NM_UE == "RIO DE JANEIRO" & CD_CARGO == 13) %>%
   select(ANO_ELEICAO, NR_CPF_CANDIDATO, SG_PARTIDO)
 
-data_wide <- data %>% 
-  pivot_wider(names_from = ANO_ELEICAO, values_from = SG_PARTIDO, names_prefix = "SG_PARTIDO_") %>%
-  filter(SG_PARTIDO_2016 == SG_PARTIDO_2020)
+data_wide <- data %>%
+  distinct(ANO_ELEICAO, NR_CPF_CANDIDATO, .keep_all = T) %>%
+  pivot_wider(names_from = ANO_ELEICAO, values_from = SG_PARTIDO, 
+              names_prefix = "SG_PARTIDO_")
+
+# tidy graph like data format
+
+data_wide %<>% filter(!is.na(SG_PARTIDO_2016) & !is.na(SG_PARTIDO_2020))
+
+connect <- data_wide %>%
+  select(SG_PARTIDO_2016, SG_PARTIDO_2020) %>%
+  mutate(VALUE = 1)
+
+nodes <- c(connect$SG_PARTIDO_2016, connect$SG_PARTIDO_2020) %>% 
+  as.tibble() %>%
+  group_by(value) %>%
+  summarise(n = n()) %>%
+  select(SG_PARTIDO = value, N = n)
+
+# VIZ
+
+
 
